04df12e305762b184343027b51a839ce4f6ec409
ElianRivoira <eli_rivoira@hotmail.com>
2023-03-27 10:32:43 -0300

    initial setup

---
+++ aidam-site/src/pages/_document.js
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon-384x384.png'></link>
+        <link rel='icon' href='/icon-384x384.png' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Roboto:wght@300&display=swap'
+          rel='stylesheet'
+        ></link>
+        <meta name='theme-color' content='#fff' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
