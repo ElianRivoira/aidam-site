1fc1c78f4326f8231d7510982f3c36f6ec9b0b33
ElianRivoira <eli_rivoira@hotmail.com>
2023-03-29 18:56:29 -0300

    modified navbar and modeloVital page

--- aidam-site/src/components/Navbar.jsx
+++ aidam-site/src/components/Navbar.jsx
@@ -10,24 +10,37 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   // smooth transition to white background pending
   return (
-    <nav className={`px-8 ${styles.gradient} h-[93px] flex justify-center`}>
-      <div className='max-w-7xl w-full flex justify-between h-full'>
-        <div className='flex items-center'>
-          <Image src={aidam} alt='aidam' height={70} className='' />
+    <nav className={`pl-8 ${styles.gradient} h-[93px] flex justify-center`}>
+      <div className='max-w-7xl w-full flex h-full justify-between'>
+        <div className='flex !w-2/3'>
+          <div className='flex items-center relative lg:!w-[197px]'>
+            <Image src={aidam} alt='aidam' height={70} className='' />
+          </div>
+          <div className='flex flex-col'>
+            <div className={`${styles.triangleBottom}`}></div>
+            <div className={`${styles.triangleTop}`}></div>
+          </div>
+          <div className='lg:w-[45%] w-0 bg-aidamBlue'></div>
+          {useMediaQuery(1024) ? (
+            <div className=' w-1/2 bg-aidamBlue'></div>
+          ) : null}
         </div>
         {useMediaQuery(1024) ? (
-          <div className={styles.container}>
-            <button
-              className={`${styles.hamburger} ${open && styles.active}`}
-              onClick={() => setOpen(!open)}
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-          </div>
+          <>
+            {/* <div className=' w-1/2 bg-aidamBlue'></div> */}
+            <div className={`${styles.container}`}>
+              <button
+                className={`${styles.hamburger} ${open && styles.active}`}
+                onClick={() => setOpen(!open)}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </>
         ) : (
-          <div className='flex gap-12 w-2/3 max-w-4xl items-center font-openSans text-lb text-aidamBlue font-medium'>
+          <div className='flex justify-around w-full !max-w-4xl items-center font-openSans text-lb text-white font-medium bg-aidamBlue'>
             <Link href={''} className={`${styles.navbarButton}`}>
               INICIO
             </Link>
@@ -46,12 +59,15 @@ const Navbar = () => {
           </div>
         )}
         {open ? (
-          <div className='absolute inset-0 z-10 bg-white/[.9] flex flex-col' style={{
-            transform: `translateY(${open ? '0' : '-100%'})`,
-            transition: 'transform 1s ease-in-out',
-          }}>
+          <div
+            className='fixed inset-0 z-40 bg-white/[.95] flex flex-col'
+            style={{
+              transform: `translateY(${open ? '0' : '-100%'})`,
+              transition: 'transform 1s ease-in-out',
+            }}
+          >
             <div className='h-[93px]'></div>
-            <div className='flex flex-col justify-center items-center mt-10 font-openSans text-lb gap-10'>
+            <div className='flex flex-col justify-center items-center mt-10 font-openSans font-medium text-lb gap-10'>
               <Link href={''} className=''>
                 INICIO
               </Link>
