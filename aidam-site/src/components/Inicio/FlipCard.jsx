ec1c5a13be23aa8884722da1ef7aa86a43e31d41
ElianRivoira <eli_rivoira@hotmail.com>
2023-03-31 18:16:06 -0300

    changed images in flip cards

--- aidam-site/src/components/Inicio/FlipCard.jsx
+++ aidam-site/src/components/Inicio/FlipCard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import styles from '@/styles/Inicio.module.css';
 
-const FlipCard = ({title, img}) => {
+const FlipCard = ({title, imgClass}) => {
   return (
     <div className={`${styles.cardContainer}`}>
       <div className={`${styles.front} flex justify-center items-center rounded-md`}>
         <h2 className='font-libreBaskerville text-3xl text-center'>{title}</h2>
       </div>
-        <div className={`${styles.back} rounded-md`}>
+        <div className={`${styles.back} rounded-md ${styles[imgClass]}`}>
       </div>
     </div>
   );
