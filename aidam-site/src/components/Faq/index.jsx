5fe99e551e59915fa40418fa0063566f2d6b109e
ElianRivoira <eli_rivoira@hotmail.com>
2023-04-03 21:49:58 -0300

    fixed faq

--- aidam-site/src/components/Faq/index.jsx
+++ aidam-site/src/components/Faq/index.jsx
@@ -8,45 +8,35 @@ import styles from '../../styles/Modelo-aidam.module.css';
 const questions = [
   {
     id: 1,
-    question: 'Question 1',
+    question: '¿Qué es un centro de día para personas mayores?',
     answer:
-      'Es un lugar especializado en el tratamiento terapéutico NO farmacológico del adulto mayor con deterioro leve y/o moderado cuyo objetivo central es promover la autonomía e independencia de nuestros asistentes.  Su ser querido encontrará en nuestro Centro un equipo de profesionales entrenado y capacitado que lo ayudará a desarrollar al máximo las capacidades y habilidades que aún permanecen activas, permitiéndole permanecer en su hogar el mayor tiempo posible, evitando y/o retrasando en el tiempo una posible institucionalización geriátrica.',
+      'Es un lugar especializado en el tratamiento terapéutico NO farmacológico de personas de 18 a 60 años con deterioro leve y/o moderado cuyo objetivo central es promover la autonomía e independencia de nuestros asistentes.  Su ser querido encontrará en nuestro Centro un equipo de profesionales entrenado y capacitado que lo ayudará a desarrollar al máximo las capacidades y habilidades.',
   },
   {
     id: 2,
-    question: 'Question 2',
+    question: '¿A quién está dirigido?',
     answer:
-      'Es un lugar especializado en el tratamiento terapéutico NO farmacológico del adulto mayor con deterioro leve y/o moderado cuyo objetivo central es promover la autonomía e independencia de nuestros asistentes.  Su ser querido encontrará en nuestro Centro un equipo de profesionales entrenado y capacitado que lo ayudará a desarrollar al máximo las capacidades y habilidades que aún permanecen activas, permitiéndole permanecer en su hogar el mayor tiempo posible, evitando y/o retrasando en el tiempo una posible institucionalización geriátrica.',
+      'Nuestra propuesta tiene probada efectividad en: - Personas con dependencia leve y/o moderada tanto física como cognitiva. - Presenten deterioro emocional. - Mayores que sientan soledad y deseen interactuar con otros en un marco de alegría y cuidados. Luego de una entrevista inicial determinamos junto con el candidato y su familia el mejor programa terapéutico adaptado siempre a sus necesidades y deseos.',
   },
   {
     id: 3,
-    question: 'Question 3',
-    answer: 'Answer 3',
+    question: '¿A partir de qué edad puedo asistir?',
+    answer: 'La propuesta del Centro de día está orientada a personas con discapacidad, con deterioro tanto leve como moderado. Los procesos de deterioro físico como cognitivo suelen comenzar. Nuestra propuesta se adapta a las necesidades de cada asistente independientemente de su edad.',
   },
   {
     id: 4,
-    question: 'Question 4',
-    answer: 'Answer 4',
+    question: '¿Qué se hace en un centro de día',
+    answer: 'Desde el paradigma de la adultez saludable con enfoque multidisciplinar e integral se abordan 6 ejes generales para mejorar la calidad de vida de nuestros pacientes: prevención y promoción de la salud, estimulación y rehabilitación cognitiva, estimulación y rehabilitación físico-funcional, estimulación Psico-Socio-Emocional, actividades lúdico-recreativas y fortalecimiento familiar.',
   },
   {
     id: 5,
-    question: 'Question 5',
-    answer: 'Answer 5',
+    question: '¿Qué ventajas tiene asistir a un Centro de día?',
+    answer: 'Los beneficios de asistir a un centro de día son múltiples, se podrían sintetizar en los siguientes aspectos: valoración gerontológica permanente, terapias preventivas personalizadas, terapias rehabilitadoras personalizadas, fortalecimiento de las capacidades sociales y entrenamiento de las habilidades remanentes.',
   },
   {
     id: 6,
-    question: 'Question 6',
-    answer: 'Answer 6',
-  },
-  {
-    id: 7,
-    question: 'Question 7',
-    answer: 'Answer 7',
-  },
-  {
-    id: 8,
-    question: 'Question 8',
-    answer: 'Answer 8',
+    question: '¿Cómo es el ingreso a un centro de día?',
+    answer: 'El proceso se inicia con un primer contacto entre el potencial asistente y el equipo interdisciplinario a los fines de conocer en detalle sus necesidades. Una vez que se acuerda el modelo terapéutico más adecuado se procede a realizar la Valoración Gerontológica Integral (VGI) para determinar el Programa de Atención Integral (PAI) personalizado.',
   },
 ];
 
@@ -66,16 +56,18 @@ const Faq = ({ customQuestions }) => {
       {customQuestions ? (
         <>
           {customQuestions.map((q, index) => {
-            const clas = 'question'+index;
-            return (<Question
-              handleQuestionClick={handleQuestionClick}
-              q={q}
-              activeQuestion={activeQuestion}
-              custom={true}
-              key={index}
-              clas={`${styles[clas]}`}
-            />
-          )})}
+            const clas = 'question' + index;
+            return (
+              <Question
+                handleQuestionClick={handleQuestionClick}
+                q={q}
+                activeQuestion={activeQuestion}
+                custom={true}
+                key={index}
+                clas={`${styles[clas]}`}
+              />
+            );
+          })}
         </>
       ) : (
         <>
@@ -88,7 +80,7 @@ const Faq = ({ customQuestions }) => {
             </div>
             <div className='flex flex-col md:flex-row mt-6 md:px-36 text-white'>
               <div className='md:w-1/2 md:pr-2'>
-                {questions.slice(0, 4).map((q, index) => (
+                {questions.slice(0, 3).map((q, index) => (
                   <Question
                     handleQuestionClick={handleQuestionClick}
                     q={q}
@@ -98,7 +90,7 @@ const Faq = ({ customQuestions }) => {
                 ))}
               </div>
               <div className='md:w-1/2 md:pl-2'>
-                {questions.slice(4).map((q, index) => (
+                {questions.slice(3).map((q, index) => (
                   <Question
                     handleQuestionClick={handleQuestionClick}
                     q={q}
