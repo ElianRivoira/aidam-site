2a13b81572d8c035f482fda79337a4d9f17fb44a
ElianRivoira <eli_rivoira@hotmail.com>
2023-04-05 12:54:28 -0300

    configured emailJS

--- aidam-site/src/services/sendEmail.js
+++ aidam-site/src/services/sendEmail.js
@@ -1,15 +1,20 @@
 import emailjs from '@emailjs/browser';
+import Router from 'next/router';
 
 export const sendEmail = (e, template) => {
   e.preventDefault();
 
-  const serviceId = 'service_1iyoqgf'; // Id de servicio de emailJS
-  const userId = 'zc5yQiZMitv3PsWY_'; // Id de usuario de emailJS
+  const serviceId = 'service_fo8col8'; // Id de servicio de emailJS
+  const userId = '7QAKyICD2Ig9av3lM'; // Id de usuario de emailJS
 
-  emailjs.sendForm(serviceId, template, e.target, userId)
-    .then((result) => {
-        console.log(result.text);
-    }, (error) => {
-        console.log(error.text);
-    });
+  emailjs.sendForm(serviceId, template, e.target, userId).then(
+    result => {
+      console.log(result.text);
+
+      Router.reload(window.location.pathname);
+    },
+    error => {
+      console.log(error.text);
+    }
+  );
 };
